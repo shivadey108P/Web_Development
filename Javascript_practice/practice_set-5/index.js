b602af4eff3ada5a0ca1282797e78f64a4f3ad64
Shiva Dey <105166536+shivadey108P@users.noreply.github.com>
2025-03-28 13:54:42 +0000

    Update index.js for chapter-5

--- Javascript_practice/practice_set-5/index.js
+++ Javascript_practice/practice_set-5/index.js
@@ -3,5 +3,23 @@
 * the use to add numbers to this array.
 */
 
-let num = prompt("Enter a number: ")
-console.log(num);
+// let num = prompt("Enter a number: ");
+// console.log(num);
+
+let arr = []
+let i = 0
+while (arr[i] == 0)
+{
+    let p = prompt("enter the array element");
+    arr.push(p);
+    i++;
+}
+
+
+console.log(arr)
+console.log(typeof arr)
+
+/*
+* Q1: Keep adding numbers to the array in 1 until 0
+* is added to the array
+*/
